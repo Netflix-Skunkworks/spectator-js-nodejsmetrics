9c3fd7fae2d3654d6e68d843def821b09a049439
Matthew Johnson <matthewj@netflix.com>
2025-03-21 20:13:02 -0400

    spectator-js thin client implementation

--- test/nodemetrics.test.js
+++ test/nodemetrics.test.js
@@ -1,70 +1,42 @@
-'use strict';
+const fs = require("fs");
+const spectator = require("nflx-spectator");
+const NodeMetrics = require("../");
+const assert = require("chai").assert;
 
-const fs = require('fs');
-const spectator = require('nflx-spectator');
-const NodeMetrics = require('../');
-const assert = require('chai').assert;
+describe("nodemetrics", () => {
 
-describe('nodemetrics', () => {
-  it('should not prevent node from exiting', () => {
-    // basic test to make sure `start()` with no `stop()`
-    // does not prevent the mocha from exiting
-    const registry = new spectator.Registry({strictMode: true, gaugePollingFrequency: 1});
+  it("should not prevent node from exiting", () => {
+    // ensure `start()` with no `stop()` does not prevent mocha from exiting
+    const config = new spectator.Config("memory");
+    const registry = new spectator.Registry(config);
     const metrics = new NodeMetrics(registry);
     metrics.start();
   });
 
-  it('should generate a few meters', (done) => {
-    // basic test to make sure `start()` actually starts the
-    // collection
-    const registry = new spectator.Registry({strictMode: true, gaugePollingFrequency: 1});
+  it("should generate a few meters", (done) => {
+    // ensure `start()` actually starts the collection
+    const config = new spectator.Config("memory");
+    const registry = new spectator.Registry(config);
     const metrics = new NodeMetrics(registry);
     metrics.start();
 
     setTimeout(() => {
-      const meters = registry.meters();
-      assert.isTrue(meters.length > 15);
+      // locally, we can see 3-12 values, mostly 3
+      assert.isTrue(registry.writer().get().length >= 3);
       metrics.stop();
       done();
-    });
+    }, 2);
   });
 
-  // ensures a timer with a count of one was correctly generated
-  function assertTimer(timer, elapsedTime) {
-    const v = elapsedTime;
-    assert.isTrue(Math.abs(timer.totalTime - v) < 1e-9, `Expected ${timer.totalTime} to be ${v}`);
-    assert.isTrue(Math.abs(timer.totalOfSquares - v * v) < 1e-9);
-    assert.isTrue(Math.abs(timer.max - v) < 1e-9);
-    assert.equal(timer.count, 1);
-  }
+  it("should collect gc metrics", () => {
+    const gcEvents = JSON.parse(fs.readFileSync("test/resources/gc-events.json").toString());
 
-  // filters the measurements to find the timer values for a given name
-  // makes sure there's only one id tag for that name
-  function getTimer(measurements, timerName) {
-    const timer = {};
-    let id;
-    for (let m of measurements) {
-      if (m.id.name === timerName) {
-        timer[m.id.tags.get('statistic')] = m.v;
-        const thisId = m.id.tags.get('id');
-        if (!id) {
-          id = thisId;
-        } else {
-          // ensure there's only one id for the timerName
-          assert.equal(id, thisId);
-        }
-      }
-    }
+    const config = new spectator.Config("memory");
+    const registry = new spectator.Registry(config);
 
-    return timer;
-  }
-
-  it('should collect gc metrics', () => {
-    const gcEvents = JSON.parse(fs.readFileSync('test/resources/gc-events.json'));
-
-    const registry = new spectator.Registry({strictMode: true, gaugePollingFrequency: 1});
     let nanos = 0;
-    registry.hrtime = () => {
+    const f = process.hrtime;
+    process.hrtime = () => {
       nanos += 1e6;
       return [0, nanos];
     };
@@ -73,25 +45,27 @@ describe('nodemetrics', () => {
 
     let mapSize;
     let largeSize;
+    let expectedLiveDataSize;
 
-    function callbackGenerator(f) {
-      for (let gcEvt of gcEvents) {
-        f(gcEvt);
-        const ms = registry.measurements();
-        let expectedLiveDataSize;
-        const oldAfter = gcEvt.after.heapSpaceStats[2].spaceUsedSize;
-        if (gcEvt.type === 'markSweepCompact') {
+    function callbackGenerator(fn) {
+      for (let gcEvent of gcEvents) {
+        fn(gcEvent);
+
+        const oldAfter = gcEvent.after.heapSpaceStats[2].spaceUsedSize;
+        if (gcEvent.type === "markSweepCompact") {
           expectedLiveDataSize = oldAfter;
         }
-        const expectedPromotionRate = oldAfter - gcEvt.before.heapSpaceStats[2].spaceUsedSize;
-        const youngAfter = gcEvt.after.heapSpaceStats[1].spaceUsedSize;
-        const youngBefore = gcEvt.before.heapSpaceStats[1].spaceUsedSize;
+
+        const expectedPromotionRate = oldAfter - gcEvent.before.heapSpaceStats[2].spaceUsedSize;
+        const youngAfter = gcEvent.after.heapSpaceStats[1].spaceUsedSize;
+        const youngBefore = gcEvent.before.heapSpaceStats[1].spaceUsedSize;
         let expectedAllocationRate = youngAfter < youngBefore ? youngBefore - youngAfter : 0;
+
         // see if we allocated something in map or large
-        const beforeMap = gcEvt.before.heapSpaceStats[4].spaceUsedSize;
-        const beforeLarge = gcEvt.before.heapSpaceStats[5].spaceUsedSize;
-        const afterMap = gcEvt.after.heapSpaceStats[4].spaceUsedSize;
-        const afterLarge = gcEvt.after.heapSpaceStats[5].spaceUsedSize;
+        const beforeMap = gcEvent.before.heapSpaceStats[4].spaceUsedSize;
+        const beforeLarge = gcEvent.before.heapSpaceStats[5].spaceUsedSize;
+        const afterMap = gcEvent.after.heapSpaceStats[4].spaceUsedSize;
+        const afterLarge = gcEvent.after.heapSpaceStats[5].spaceUsedSize;
         if (mapSize && mapSize < beforeMap) {
           expectedAllocationRate += beforeMap - mapSize;
         }
@@ -100,96 +74,114 @@ describe('nodemetrics', () => {
           expectedAllocationRate += beforeLarge - largeSize;
         }
         largeSize = afterLarge;
-        for (let m of ms) {
-          assert.equal(m.id.tags.get('nodejs.version'), process.version);
-          if (m.id.name === 'nodejs.gc.maxDataSize') {
-            assert.equal(m.v, 1526909922);
-          } else if (m.id.name === 'nodejs.gc.liveDataSize') {
-            assert.equal(m.v, expectedLiveDataSize);
-          } else if (m.id.name === 'nodejs.gc.promotionRate') {
-            assert.equal(m.v, expectedPromotionRate);
-          } else if (m.id.name === 'nodejs.gc.allocationRate') {
-            assert.equal(m.v, expectedAllocationRate);
-          } else if (m.id.name !== 'nodejs.gc.pause') {
-            assert.fail(`Unexpected measurement generated: ${m.id.key}=${m.v}`);
+
+        for (let line of registry.writer().get()) {
+          const [, id, value] = spectator.parse_protocol_line(line);
+
+          assert.equal(id.tags()["nodejs.version"], process.version);
+
+          if (id.name() === "nodejs.gc.maxDataSize") {
+            assert.equal(parseFloat(value), 1526909922, "maxDataSize does not match");
+          } else if (id.name() === "nodejs.gc.liveDataSize") {
+            assert.equal(parseFloat(value), expectedLiveDataSize, "liveDataSize does not match");
+          } else if (id.name() === "nodejs.gc.promotionRate") {
+            assert.equal(parseFloat(value), expectedPromotionRate, "promotionRate does not match");
+          } else if (id.name() === "nodejs.gc.allocationRate") {
+            assert.equal(parseFloat(value), expectedAllocationRate, "allocationRate does not match");
+          } else if (id.name() === "nodejs.gc.pause") {
+            assert.equal(parseFloat(value), gcEvent.elapsed, "pause does not match");
+          } else if (id.name() !== "nodejs.gc.pause") {
+            assert.fail(`Unexpected protocol line: ${line}`);
           }
         }
-        const gcPauseTimer = getTimer(ms, 'nodejs.gc.pause');
-        assertTimer(gcPauseTimer, gcEvt.elapsed);
+        registry.writer().clear();
       }
     }
 
     metrics._gcEvents(callbackGenerator);
+
+    process.hrtime = f;
   });
 
-  it('should collect fd metrics', () => {
-    const registry = new spectator.Registry({strictMode: true, gaugePollingFrequency: 1});
+  it("should collect fd metrics", () => {
+    const config = new spectator.Config("memory");
+    const registry = new spectator.Registry(config);
     const metrics = new NodeMetrics(registry);
 
-    function assertFd(measurements, open, max) {
-      for (let m of registry.measurements()) {
-        if (m.id.name === 'openFileDescriptorsCount') {
-          assert.equal(m.v, open);
-        } else if (m.id.name === 'maxFileDescriptorsCount') {
-          assert.equal(m.v, max);
+    function assertFd(open, max) {
+      for (let line of registry.writer().get()) {
+        const [, id, value] = spectator.parse_protocol_line(line);
+        if (id.name() === "openFileDescriptorsCount") {
+          assert.equal(value, open, "openFileDescriptorsCount does not match");
+        } else if (id.name() === "maxFileDescriptorsCount") {
+          assert.equal(value, max, "maxFileDescriptorsCount does not match");
         } else {
-          assert.fail(`Unexpected measurement: ${m.id.key}=${m.v}`);
+          assert.fail(`Unexpected protocol line: ${line}`);
         }
       }
+      registry.writer().clear();
     }
 
     NodeMetrics.updateFdGauges(metrics, () => {
       return {used: 42, max: 32768};
     });
-    assertFd(registry.measurements(), 42, 32768);
+    assertFd(42, 32768);
 
     NodeMetrics.updateFdGauges(metrics, () => {
       return {used: 1, max: 1024};
     });
-    assertFd(registry.measurements(), 1, 1024);
+    assertFd(1, 1024);
 
     // test max == null (which shouldn't produce a metric)
     NodeMetrics.updateFdGauges(metrics, () => {
       return {used: 1, max: null};
     });
-    assertFd(registry.measurements(), 1);
+    assertFd(1);
   });
 
-  it('should collect evt loop lag time', () => {
-    const registry = new spectator.Registry({strictMode: true, gaugePollingFrequency: 1});
+  it("should collect event loop lag time", () => {
+    const config = new spectator.Config("memory");
+    const registry = new spectator.Registry(config);
     const metrics = new NodeMetrics(registry);
 
     let nanos = 0;
     let round = 1;
-    registry.hrtime = () => {
-      nanos += 1e9 + round * 1e6; // 1ms lag first time, 2ms second time, etc.
+    const f = process.hrtime;
+    process.hrtime = () => {
+      nanos += 1e9 + round * 1e6;  // 1ms lag first time, 2ms second time, etc.
       ++round;
       return [0, nanos];
     };
 
     metrics._lastNanos = 0;
 
-    NodeMetrics.updateEvtLoopLag(metrics);
-    const timer = metrics.evtLoopLagTimer;
-    let t = timer.totalTime / 1e9;
-    assert.closeTo(t, 0.001, 1e-6);
-    assert.equal(timer.count, 1);
+    NodeMetrics.updateEventLoopLag(metrics);
+    assert.equal(registry.writer().get().length, 1);
+    let [, id, value] = spectator.parse_protocol_line(registry.writer().get()[0]);
+    assert.equal(id.name(), "nodejs.eventLoopLag");
+    assert.equal(id.tags()["nodejs.version"], process.version);
+    assert.closeTo(parseFloat(value), 0.001, 1e-6);
+    registry.writer().clear();
 
-    NodeMetrics.updateEvtLoopLag(metrics);
-    t = timer.totalTime / 1e9;
-    assert.closeTo(t, 0.003, 1e-6); // 1ms + 2ms
-    assert.equal(timer.count, 2);
+    NodeMetrics.updateEventLoopLag(metrics);
+    assert.equal(registry.writer().get().length, 1);
+    [, , value] = spectator.parse_protocol_line(registry.writer().get()[0]);
+    assert.closeTo(parseFloat(value), 0.002, 1e-6);
+    registry.writer().clear();
 
-    timer.measure(); // reset
-    NodeMetrics.updateEvtLoopLag(metrics);
-    t = timer.totalTime / 1e9;
-    assert.closeTo(t, 0.003, 1e-6);
-    assert.equal(timer.count, 1);
+    NodeMetrics.updateEventLoopLag(metrics);
+    assert.equal(registry.writer().get().length, 1);
+    [, , value] = spectator.parse_protocol_line(registry.writer().get()[0]);
+    assert.closeTo(parseFloat(value), 0.003, 1e-6);
+    registry.writer().clear();
+
+    process.hrtime = f;
   });
 
-  it('should collect eventLoopUtilization metrics when possible', () => {
+  it("should collect eventLoopUtilization metrics when possible", () => {
+    const config = new spectator.Config("memory");
+    const registry = new spectator.Registry(config);
 
-    const registry = new spectator.Registry({strictMode: true, gaugePollingFrequency: 1});
     const metrics = new NodeMetrics(registry);
     metrics.lastEventLoopTime = [0, 0];
     metrics.lastEventLoop = {
@@ -208,35 +200,46 @@ describe('nodemetrics', () => {
       return Object.assign({}, elu);
     };
 
+    const f = process.hrtime;
     let seconds = 3;
-    registry.hrtime = () => {
+    process.hrtime = () => {
       return [seconds, 0];
     };
 
-    NodeMetrics.measureEvtLoopUtilization(metrics);
-
-    const active = metrics.eventLoopActive;
-    assert.closeTo(active.get(), 200 / 3.0, 1e-6);
+    NodeMetrics.measureEventLoopUtilization(metrics);
+    assert.equal(registry.writer().get().length, 1);
+    let [, id, value] = spectator.parse_protocol_line(registry.writer().get()[0]);
+    assert.equal(id.name(), "nodejs.eventLoopUtilization");
+    assert.equal(id.tags()["nodejs.version"], process.version);
+    assert.closeTo(parseFloat(value), 200 / 3.0, 1e-6);
+    registry.writer().clear();
 
     // 5s, 1s active, 4s idle
     seconds += 5;
     elu.idle += 4000;
     elu.active += 1000;
     elu.utilization = 1 / 5.0;
-    NodeMetrics.measureEvtLoopUtilization(metrics);
-    assert.closeTo(active.get(), 100 / 5.0, 1e-6);
+
+    NodeMetrics.measureEventLoopUtilization(metrics);
+    assert.equal(registry.writer().get().length, 1);
+    [, , value] = spectator.parse_protocol_line(registry.writer().get()[0]);
+    assert.closeTo(parseFloat(value), 100 / 5.0, 1e-6);
+
+    process.hrtime = f;
   });
 
-  it('should provide a way to check whether it has started', () => {
-    const registry = new spectator.Registry({strictMode: true, gaugePollingFrequency: 1});
+  it("should provide a way to check whether it has started", () => {
+    const config = new spectator.Config("memory");
+    const registry = new spectator.Registry(config);
     const metrics = new NodeMetrics(registry);
 
     assert.isFalse(metrics.started);
     metrics.start();
     assert.isTrue(metrics.started);
-    // should do nothing
-    metrics.start();
+
+    metrics.start();  // does nothing
     assert.isTrue(metrics.started);
+
     metrics.stop();
     assert.isFalse(metrics.started);
   });
